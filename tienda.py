c39ffd513bbf4368cd8ca9c772f02cb82af54a95
Majo <monterosmarijo2004@gmil.com>
2025-08-22 17:03:46 -0500

    Validación de la opción 2. Agregar al carrito

--- tienda.py
+++ tienda.py
@@ -1,6 +1,7 @@
-# Catálogo 
-
+ 
 catalogo = []
+carrito = []  
+
 def agg_prod (codigo, nombre, precio, stock):
     producto = {
         "codigo": codigo,
@@ -17,30 +18,61 @@ agg_prod("P004", "Fideos", 0.75, 100)
 agg_prod("P005", "Cola", 1.00, 100)
 agg_prod("P006", "Jugo", 0.50, 100)
 
-#Menú
 
-print("""---Bienvenido---
+while True:
+    print("""---Bienvenido---
 Escoja una opción:
 1. Ver catálogo
 2. Agregar al carrito
 3. Ver carrito
 4. finalizar compra
 0. Salir
-      """)
-
-opcion = int(input("Ingrese la opción: "))
-
-if opcion == 1:
-    
-    for prod in catalogo:
-        print(f"\n{prod['codigo']} | {prod['nombre']} |  {prod['precio']} | {prod['stock']}")
-        
-    print("""\nEscoja una opción: \n
-        2. Agregar al carrito
-        3. Ver carrito
-        4. finalizar compra
-        0. Salir
-            """)
+""")
     
     opcion = int(input("Ingrese la opción: "))
-
+    if opcion == 1:
+        
+        for prod in catalogo:
+            print(f"\n{prod['codigo']} | {prod['nombre']} |  {prod['precio']} | {prod['stock']}")
+            
+        while True:
+            volver = input("\nIngrese 9 para regresar al menú principal: ")
+            if volver == "9":
+                break
+            
+        
+    elif opcion == 2:
+            
+        codigo_buscar = input("Agrege el codigo del producto que desea agregar: ").upper()
+            
+        producto_encontrado = None
+            
+        for prod in catalogo:
+            if prod["codigo"] == codigo_buscar:
+                producto_encontrado = prod
+                break
+            
+        if producto_encontrado:
+            cantidad = int(input("Ingrese la cantidad: "))
+                    
+            if cantidad <= 0:
+                print("La cantidad debe ser mayor a 0")
+            elif cantidad > producto_encontrado["stock"]:
+                print(f"No hay suficiente en stock. Disponible: {producto_encontrado['stock']}")
+            else:
+                carrito.append({
+                    "codigo": producto_encontrado["codigo"],
+                    "nombre": producto_encontrado["nombre"],
+                    "precio": producto_encontrado["precio"],
+                    "cantidad": cantidad
+                })
+                producto_encontrado["stock"] -= cantidad
+                print(f"{cantidad} unidades de {producto_encontrado['nombre']} se agregaron al carrito")
+        
+        else: 
+            print("Producto no encontrado ")  
+            
+        while True:
+            volver = input("\nIngrese 9 para regresar al menú principal: ")
+            if volver == "9":
+                break   
