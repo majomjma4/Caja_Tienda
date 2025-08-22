b14c17a37d56b1def354e4dc73eb30bdee7e0ebf
Majo <monterosmarijo2004@gmil.com>
2025-08-22 13:17:06 -0500

    Menú de Opciones

---
+++ tienda.py
@@ -0,0 +1,13 @@
+
+#Menú
+
+print("""---Bienvenido---
+Escoja una opción:
+1. Ver catálogo
+2. Agregar al carrito
+3. Ver carrito
+4. finalizar compra
+0. Salir
+      """)
+
+opcion = int(input("Ingrese la opción: "))
