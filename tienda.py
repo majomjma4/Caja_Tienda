e84d8960dd5c9d3e001a41bd32c5779236a4f4fb
Majo <monterosmarijo2004@gmil.com>
2025-08-22 13:35:24 -0500

    Creación del catálogo con 6 productos

--- tienda.py
+++ tienda.py
@@ -1,3 +1,21 @@
+# Catálogo 
+
+catalogo = []
+def agg_prod (codigo, nombre, precio, stock):
+    producto = {
+        "codigo": codigo,
+        "nombre": nombre,
+        "precio": precio,
+        "stock": stock
+    }
+    catalogo.append(producto)
+
+agg_prod("P001", "Azucar", 2.50, 100)
+agg_prod("P002", "Sal", 1.00, 100)
+agg_prod("P003", "Atún", 1.65, 100)
+agg_prod("P004", "Fideos", 0.75, 100)
+agg_prod("P005", "Cola", 1.00, 100)
+agg_prod("P006", "Jugo", 0.50, 100)
 
 #Menú
 
@@ -11,3 +29,4 @@ Escoja una opción:
       """)
 
 opcion = int(input("Ingrese la opción: "))
+
